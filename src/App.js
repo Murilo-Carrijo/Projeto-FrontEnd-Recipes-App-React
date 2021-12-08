a61d1fe0d8eb289d6e825668a63df3f2808b0070
Leonardo Monti <contatoleomonti@gmail.com>
2021-12-08 17:26:25 -0300

    adiciona pages login, foods e drinks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router';
+import Provider from './context/MyProvider';
+import Drinks from './pages/Drinks';
+import Foods from './pages/Foods';
+import Login from './pages/Login';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ Foods } />
+        <Route path="/bebidas" component={ Drinks } />
+      </Switch>
+    </Provider>
   );
 }
 

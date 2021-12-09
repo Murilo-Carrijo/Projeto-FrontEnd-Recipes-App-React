afaeb3a1229167301a69ed8a4fc5ac46a229b5a9
Leonardo Monti <contatoleomonti@gmail.com>
2021-12-09 17:36:17 -0300

    renderiza footer em pages

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 function Drinks() {
   return (
-    <h1>Drinks</h1>
+    <>
+      <h1>Drinks</h1>
+      <Footer />
+    </>
   );
 }
 

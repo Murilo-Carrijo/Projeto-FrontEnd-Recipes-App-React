607ee4368a91ac7204b488fab2da1c388f81ed46
Leonardo Monti <contatoleomonti@gmail.com>
2022-01-07 18:00:21 -0300

    adiciona filtro de categorias em drinks

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,62 +1,134 @@
 import React, { useContext, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { drinksForName } from '../servises/fetchApi';
+import requestCategories from '../servises/requestCategories';
 import MyContext from '../context/MyContext';
 import Footer from '../components/Footer';
 import requestDrinks from '../servises/requestDrinks';
+import CategoriesButtons from '../components/CategoriesButtons';
+import {
+  requestDrinksByName,
+  requestDrinksFilterCategories,
+  requestDrinksListCategories,
+} from '../servises/fetchDrinks';
 
 function Drinks() {
   const {
     results,
     setResults,
     filterInfo,
+    categoriesDrinks,
+    setCategoriesDrinks,
+    selectCategories,
+    resultsCategories,
+    setResultsCategories,
+    drinksRecipes,
+    setDrinksRecipes,
   } = useContext(MyContext);
 
   const maxlength = 12;
+  const MAX_CATEGORIES = 5;
 
   useEffect(() => {
-    drinksForName('').then((recipes) => { if (recipes) { return setResults(recipes); } });
-  }, [setResults]);
+    requestDrinksByName('').then((recipes) => {
+      if (recipes) {
+        setResults(recipes);
+        setDrinksRecipes(recipes);
+      }
+    });
+  }, [setResults, setDrinksRecipes]);
 
   function handleResults(i) {
     if (i) {
-      setResults(i);
+      setDrinksRecipes(i);
     } else {
       global.alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
     }
   }
 
   useEffect(() => {
-    requestDrinks(filterInfo, results, setResults, handleResults);
-  }, [filterInfo.radio, filterInfo.text, setResults]);
+    requestDrinks(filterInfo, drinksRecipes, setResults, handleResults);
+  }, [filterInfo.radio, filterInfo.text, setDrinksRecipes]);
 
-  function setDrinks(food) {
+  function handleRecipeDrinks(drink) {
     return (
-      <div key={ food.idDrink } data-testid={ `${results.indexOf(food)}-recipe-card` }>
-        <p data-testid={ `${results.indexOf(food)}-card-name` }>{food.strDrink}</p>
+      <div
+        key={ drink.idDrink }
+        data-testid={ `${drinksRecipes.indexOf(drink)}-recipe-card` }
+      >
+        <p
+          data-testid={ `${drinksRecipes.indexOf(drink)}-card-name` }
+        >
+          {drink.strDrink}
+        </p>
         <img
-          src={ food.strDrinkThumb }
-          alt={ food.strDrink }
+          src={ drink.strDrinkThumb }
+          alt={ drink.strDrink }
           width="161"
-          data-testid={ `${results.indexOf(food)}-card-img` }
+          data-testid={ `${drinksRecipes.indexOf(drink)}-card-img` }
         />
       </div>
     );
   }
 
-  if (results.length !== null && results.length === 1) {
-    return <Redirect to={ `/bebidas/${results[0].idDrink}` } />;
+  function handleCategories(drink) {
+    return (
+      <div
+        key={ drink.idDrink }
+        data-testid={ `${resultsCategories.indexOf(drink)}-recipe-card` }
+      >
+        <p
+          data-testid={ `${resultsCategories.indexOf(drink)}-card-name` }
+        >
+          {drink.strDrink}
+        </p>
+        <img
+          src={ drink.strDrinkThumb }
+          alt={ drink.strDrink }
+          width="161"
+          data-testid={ `${resultsCategories.indexOf(drink)}-card-img` }
+        />
+      </div>
+    );
+  }
+
+  function renderResultsCategories() {
+    return resultsCategories.map((result) => (
+      resultsCategories.indexOf(result) < maxlength && handleCategories(result)));
+  }
+
+  useEffect(() => {
+    requestCategories(requestDrinksListCategories, setCategoriesDrinks, MAX_CATEGORIES);
+  }, []);
+
+  useEffect(() => {
+    if (selectCategories !== '') {
+      requestDrinksFilterCategories(selectCategories).then((response) => {
+        setResultsCategories(response);
+        setDrinksRecipes([]);
+      });
+    } else {
+      setDrinksRecipes(results);
+    }
+  }, [setResultsCategories, selectCategories]);
+
+  function validate() {
+    if (drinksRecipes.length === 1) {
+      return selectCategories ? setResultsCategories(results)
+        : <Redirect to={ `/bebidas/${drinksRecipes[0].idDrink}` } />;
+    }
   }
 
   return (
     <section>
+      {categoriesDrinks && <CategoriesButtons listCategories={ categoriesDrinks } />}
       <div>
-        {results ? results.map((result) => (
-          results.indexOf(result) < maxlength && setDrinks(result)))
+        {drinksRecipes ? drinksRecipes.map((result) => (
+          drinksRecipes.indexOf(result) < maxlength && handleRecipeDrinks(result)))
           : global.alert(
             'Sinto muito, não encontramos nenhuma receita para esses filtros.',
           )}
-        ;
+        {resultsCategories && renderResultsCategories()}
+        {validate()}
       </div>
       <Footer />
     </section>

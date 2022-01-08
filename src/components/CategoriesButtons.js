cee5a7d2282c5ad99a82654dc452cdbc604daa4d
Leonardo Monti <contatoleomonti@gmail.com>
2022-01-08 00:04:53 -0300

    adicionando funcionalidade para botao "ALL"

--- src/components/CategoriesButtons.js
+++ src/components/CategoriesButtons.js
@@ -1,12 +1,39 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from '../context/MyContext';
+import { requestDrinksByName } from '../servises/fetchDrinks';
+import { requestFoodsByName } from '../servises/fetchFoods';
 
-function CategoriesButtons({ listCategories }) {
-  const { selectCategories, setSelectCategories } = useContext(MyContext);
+function CategoriesButtons({ listCategories, recipeType }) {
+  const {
+    setDrinksRecipes,
+    setFoodsRecipes,
+    selectCategories,
+    setSelectCategories,
+  } = useContext(MyContext);
+
+  async function getAll(isfetch, setstate) {
+    const allRecipe = await isfetch('');
+    setstate(allRecipe);
+  }
+
+  function handleButtonClick() {
+    if (recipeType === 'drink') {
+      getAll(requestDrinksByName, setDrinksRecipes);
+    } else {
+      getAll(requestFoodsByName, setFoodsRecipes);
+    }
+  }
 
   return (
     <>
+      <button
+        type="button"
+        data-testid="All-category-filter"
+        onClick={ handleButtonClick }
+      >
+        All
+      </button>
       {listCategories.map((category, index) => (
         <button
           key={ index }
@@ -29,6 +56,7 @@ function CategoriesButtons({ listCategories }) {
 
 CategoriesButtons.propTypes = {
   listCategories: PropTypes.arrayOf(PropTypes.object).isRequired,
+  recipeType: PropTypes.string.isRequired,
 };
 
 export default CategoriesButtons;

e1e390d7544ad7324b235553100aff7d32897d7b
Leonardo Monti <contatoleomonti@gmail.com>
2022-01-08 18:16:34 -0300

    adiciona botoes e caminho para respectivos url

--- src/pages/ExploreDrink.js
+++ src/pages/ExploreDrink.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { requestDrinksRandomRecipe } from '../servises/fetchDrinks';
 
 function ExploreDrink() {
+  const history = useHistory();
+
+  function pathRedirect(path) {
+    history.push(path);
+  }
+
+  async function handleDrinkRandom() {
+    const response = await requestDrinksRandomRecipe();
+    const { idDrink } = response[0];
+
+    pathRedirect(`/bebidas/${idDrink}`);
+  }
+
   return (
     <>
-      <h1>Explore Drink</h1>
+      <h3>Explore Drink</h3>
+      <section>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => pathRedirect('bebidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => handleDrinkRandom() }
+        >
+          Me Surpreenda!
+        </button>
+      </section>
       <Footer />
     </>
   );
--- src/pages/ExploreFood.js
+++ src/pages/ExploreFood.js
@@ -1,10 +1,48 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
+import { requestFoodsRandomRecipe } from '../servises/fetchFoods';
 
 function ExploreFood() {
+  const history = useHistory();
+
+  function pathRedirect(path) {
+    history.push(path);
+  }
+
+  async function handleFoodsRandom() {
+    const response = await requestFoodsRandomRecipe();
+    const { idMeal } = response[0];
+
+    pathRedirect(`/comidas/${idMeal}`);
+  }
+
   return (
     <>
-      <h1>Explore Food</h1>
+      <h3>Explore Food</h3>
+      <section>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => pathRedirect('comidas/ingredientes') }
+        >
+          Por Ingredientes
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-area"
+          onClick={ () => pathRedirect('comidas/area') }
+        >
+          Por Local de Origem
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ () => handleFoodsRandom() }
+        >
+          Me Surpreenda!
+        </button>
+      </section>
       <Footer />
     </>
   );
